eff3efc2df9eefed4237aaf7a1f94ac728e94d83
omkar vijay <omkarvijay5@gmail.com>
2020-12-24 18:05:12 +0530

    updated the payload and few validations on the request data

--- app.py
+++ app.py
@@ -1,42 +1,72 @@
 from services.shopping_cart import calculate_total
 
-from flask import Flask, request
+from flask import Flask, request, Response
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=["POST"])
 def shopping_cart():
     """
     example payload
     payload = {
-        'currency_rate': 1,
-        'shipping_charge': 0,
-        'products': [
+        "currency_rate": 1,
+        "products": [
             {
-                'name': 'T-shirt', 'price': 500,
-                'discounts': [],
-                'quantity': 2,
-                'taxes': [{'amount': 18, 'type':'percentage'}]
-            },
-            {
-                'name': 'Jacket', 'price': 2500,
-                'discounts': [
-                    {'amount': '50', 'type': 'percentage'}
+                "discounts": [
                 ],
-                'quantity': 1,
-                'taxes': [{'amount': 18, 'type':'percentage'}]
-             },
-            {
-                'name': 'Shoes', 'price': 5000,
-                'discounts': [{'amount': '10', 'type':'percentage'}], 'quantity': 1,
-                'taxes': [{'amount': 18, 'type': 'percentage'}]
+                "name": "T-shirt",
+                "price": 500,
+                "quantity": 2,
+                "taxes": [
+                    {
+                        "amount": 18,
+                        "type": "percentage"
+                    }
+                ]
             },
-        ]
+            {
+                "discounts": [
+                    {
+                        "amount": "50",
+                        "type": "percentage"
+                    }
+                ],
+                "name": "Jacket",
+                "price": 2500,
+                "quantity": 1,
+                "taxes": [
+                    {
+                        "amount": 18,
+                        "type": "percentage"
+                    }
+                ]
+            },
+            {
+                "discounts": [
+                    {
+                        "amount": "10",
+                        "type": "percentage"
+                    }
+                ],
+                "name": "Shoes",
+                "price": 5000,
+                "quantity": 1,
+                "taxes": [
+                    {
+                        "amount": 18,
+                        "type": "percentage"
+                    }
+                ]
+            }
+        ],
+        "shipping_charge": 0
     }
     :return: cart total
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         payload = request.get_json()
+        if 'products' not in payload:
+            return Response("invalid request", status=400)
         total = str(calculate_total(payload))
         return total
 
